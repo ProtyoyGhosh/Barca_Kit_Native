3bb971636512b8a20cf13ec61b109233dc653e97
ProtyoyGhosh <pghosh0615@gmail.com>
2021-12-17 11:52:28 +0600

    getting started

--- App.js
+++ App.js
@@ -4,7 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.kitContainer}>
+        <View style={styles.titles}>
+          <Text style={styles.title}>Home Kit 2021</Text>
+          <Text style={styles.subTitle}>Starting at $59.00</Text>
+        </View>
+      </View>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'violet',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  kitContainer: {
+    height: "100%",
+    width: "100%"
+  },
+  titles: {
+    marginTop: "30%",
+    width: "100%",
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold'
+  },
+  subTitle: {
+    fontSize: 16,
+    color: 'blue'
+  }
 });
